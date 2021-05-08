e5e747df4136a063b8ebb81ac43cd764bcbc5d78
MrZloHex <zlo.alex.it@gmail.com>
2021-05-09 02:37:27 +0300

    good start

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 
 using namespace sf;
 
-const short size_field = 0xC;
-const short size_pixel = 0x19;
+const int size_field = 0xC;
+const int size_pixel = 0x19;
 
 
-void null_field(unsigned short field[0xC][0xC]) {
-    for (short i = 0; i < 0xD; i++) 
-        for (short j = 0; j < 0xD; j++) {
+void null_field(unsigned short field[size_field][size_field]) {
+    for (short i = 0; i < 0xC; i++) 
+        for (short j = 0; j < 0xC; j++) {
             field[i][j] = 0;
         }
 }
@@ -48,7 +48,18 @@ int main() {
                     window.close();
         }
 
-        window.clear();
+        // window.clear();
+
+        for (short x = 0; x < 0xC; x++)
+            for (short y = 0; y < 0xC; y++) {
+                RectangleShape sqaure(Vector2i(size_pixel-2, size_pixel-2));
+            
+                if (field[x][y] == 0) sqaure.setFillColor(Color(0xF0, 0xF0, 0xF0));
+
+                sqaure.setPosition((x*offset)+offset, (y*offset)+offset);
+                window.draw(sqaure);
+            }
+
         // window.draw(shape);
         window.display();
     }
