23ed379a3141e07313a462d2f4955947a4575b8d
MrZloHex <zlo.alex.it@gmail.com>
2021-05-09 03:18:41 +0300

    generating map

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,41 @@ using namespace sf;
 const short size_field = 0xC;
 const short size_pixel = 0x19;
 
+const bool dev = true; // false - normal mode; true - dev mode
 
-void null_field(unsigned short field[size_field][size_field]) {
-    for (short i = 0; i < 0xC; i++) 
-        for (short j = 0; j < 0xC; j++) {
-            field[i][j] = 8;
-        }
+
+void init_field(unsigned short field[size_field][size_field], bool type) {
+    if (type) {
+        for (short x = 0; x < 0xC; x++) 
+            for (short y = 0; y < 0xC; y++) {
+                if (rand()%0x5 == 0) field[x][y] = 0x6;
+                else field[x][y] = 0;
+            }
+    }
+    else {
+        for (short x = 0; x < 0xC; x++) 
+            for (short y = 0; y < 0xC; y++) {
+                field[x][y] = 8;
+            }
+    }
+}
+
+
+void generate_map(unsigned short field[size_field][size_field]) {
+    for (short x = 0; x < size_field; x++)
+        for (short y = 0; y < size_field; y++) {
+            unsigned short amount_bomb = 0;
+            if (field[x][y] == 6) continue;
+            if (field[x-1][y-1] == 6) amount_bomb++;
+            if (field[x-1][y] == 6) amount_bomb++;
+            if (field[x-1][y+1] == 6) amount_bomb++;
+            if (field[x][y+1] == 6) amount_bomb++;
+            if (field[x+1][y+1] == 6) amount_bomb++;
+            if (field[x+1][y] == 6) amount_bomb++;
+            if (field[x+1][y-1] == 6) amount_bomb++;
+            if (field[x][y-1] == 6) amount_bomb++;
+            field[x][y] = amount_bomb;
+        }   
 }
 
 
@@ -34,9 +63,10 @@ int main() {
     unsigned short field[size_field][size_field];
     unsigned short show_field[size_field][size_field];
 
-    null_field(field);
-    null_field(show_field);
+    init_field(field, true);
+    init_field(show_field, false);
 
+    generate_map(field);
 
     while (window.isOpen()) {
         Event event;
@@ -55,16 +85,33 @@ int main() {
             for (short y = 0; y < 0xC; y++) {
                 RectangleShape sqaure(Vector2f(size_pixel-2, size_pixel-2));
 
-                //                                               R     G     B
-                if (field[x][y] == 0) sqaure.setFillColor(Color(0x1D, 0x1D, 0x1D));
-                if (field[x][y] == 1) sqaure.setFillColor(Color(0x00, 0x80, 0xFF));
-                if (field[x][y] == 2) sqaure.setFillColor(Color(0x3A, 0xFF, 0x3A));
-                if (field[x][y] == 3) sqaure.setFillColor(Color(0xFF, 0xFF, 0x00));
-                if (field[x][y] == 4) sqaure.setFillColor(Color(0xFF, 0x3A, 0x3A));
-                if (field[x][y] == 5) sqaure.setFillColor(Color(0x80, 0x00, 0xFF));
-                if (field[x][y] == 6) sqaure.setFillColor(Color(0x00, 0x00, 0x00));
-                if (field[x][y] == 7) sqaure.setFillColor(Color(0xFF, 0xFF, 0xFF));
-                if (field[x][y] == 8) sqaure.setFillColor(Color(0xD1, 0xD1, 0xD1));
+                if (dev) {
+
+                    //                                               R     G     B
+                    if (field[x][y] == 0) sqaure.setFillColor(Color(0x5D, 0x5D, 0x5D));
+                    if (field[x][y] == 1) sqaure.setFillColor(Color(0x00, 0x80, 0xFF));
+                    if (field[x][y] == 2) sqaure.setFillColor(Color(0x3A, 0xFF, 0x3A));
+                    if (field[x][y] == 3) sqaure.setFillColor(Color(0xFF, 0xFF, 0x00));
+                    if (field[x][y] == 4) sqaure.setFillColor(Color(0xFF, 0x3A, 0x3A));
+                    if (field[x][y] == 5) sqaure.setFillColor(Color(0x80, 0x00, 0xFF));
+                    if (field[x][y] == 6) sqaure.setFillColor(Color(0x00, 0x00, 0x00));
+                    if (field[x][y] == 7) sqaure.setFillColor(Color(0xFF, 0xFF, 0xFF));
+                    if (field[x][y] == 8) sqaure.setFillColor(Color(0xD1, 0xD1, 0xD1));
+
+                }
+
+                else if (!dev) {
+                    //                                                    R     G     B
+                    if (show_field[x][y] == 0) sqaure.setFillColor(Color(0x1D, 0x1D, 0x1D));
+                    if (show_field[x][y] == 1) sqaure.setFillColor(Color(0x00, 0x80, 0xFF));
+                    if (show_field[x][y] == 2) sqaure.setFillColor(Color(0x3A, 0xFF, 0x3A));
+                    if (show_field[x][y] == 3) sqaure.setFillColor(Color(0xFF, 0xFF, 0x00));
+                    if (show_field[x][y] == 4) sqaure.setFillColor(Color(0xFF, 0x3A, 0x3A));
+                    if (show_field[x][y] == 5) sqaure.setFillColor(Color(0x80, 0x00, 0xFF));
+                    if (show_field[x][y] == 6) sqaure.setFillColor(Color(0x00, 0x00, 0x00));
+                    if (show_field[x][y] == 7) sqaure.setFillColor(Color(0xFF, 0xFF, 0xFF));
+                    if (show_field[x][y] == 8) sqaure.setFillColor(Color(0xD1, 0xD1, 0xD1));
+                }
 
                 sqaure.setPosition((x*offset), (y*offset));
                 window.draw(sqaure);
