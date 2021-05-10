9b55d6a7206552ab1af3488ff78984f218b4e804
MrZloHex <zlo.alex.it@gmail.com>
2021-05-10 15:50:07 +0300

    refactor on classes

--- src/Main.cpp
+++ src/Main.cpp
@@ -47,6 +47,16 @@ int main() {
         }
 
         // update frame
+        for (short x = 0; x < algorithm.get_size_field(); x++)
+            for (short y = 0; y < algorithm.get_size_field(); y++) {
+                //  RectangleShape square(Vector2i(algorithm.get_size_cell()-2, algorithm.get_size_cell()-2));
+
+                //square.setFillColor(Color()));
+
+                //square.setPosition((x*algorithm.get_size_cell()), (y*algorithm.get_size_cell()));
+                //window.draw(square);
+            }
+
         illustration.test();
 
         window.display();
